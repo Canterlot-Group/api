c51623ca590aaafdafc7c9d2684eacab9bfc3d53
noskla <alis@krutt.org>
2020-11-05 07:03:51 +0100

    Add validations and accountType column that represents user permissions

--- src/models/account.ts
+++ src/models/account.ts
@@ -1,30 +1,45 @@
-import {Table, Column, Model, CreatedAt, BeforeCreate} from 'sequelize-typescript';
+import {Table, Column, Model, Default, CreatedAt, BeforeCreate} from 'sequelize-typescript';
+import {IsUUID, Length, IsAlphanumeric, IsEmail, IsIn} from 'sequelize-typescript';
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
 
 @Table
 export default class Account extends Model<Account> {
 
+  @IsUUID(4)
   @Column( {primaryKey: true} )
   id?: string;
 
+  @IsAlphanumeric
+  @Length({min: 4, max: 16})
   @Column
   name!: string;
 
+  @IsEmail
+  @Length({min: 6, max: 32})
   @Column
   email!: string;
 
+  @Length({min: 6, max: 64})
   @Column
   password!: string;
 
+  @Default(true)
   @Column
   loginEnabled!: boolean;
 
+  @Default(null)
   @Column
   bannedUntil?: Date | null; // date, or null if not banned
 
+  @Default(null)
   @Column
-  bannedReason?: string | null;
+  bannedReason?: string | null; // reason, or null if not banned
+
+  @Default('regular')
+  @IsIn([['regular', 'member', 'administrator']])
+  @Column
+  accountType!: string;
 
   @CreatedAt
   @Column
