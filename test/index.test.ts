b520d8fbaa2d4572e0b0d1285047dca45488f9dd
noskla <alis@krutt.org>
2020-11-16 15:19:36 +0100

    Remove GET / from tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,9 @@
-import {expect} from 'chai';
-import app from '../src/app';
-import {agent as request} from 'supertest';
 import sequelize from '../src/database';
 
 describe('Index Init Test', () => {
 
-  beforeEach(done => {
+  it('should synchronize the database', done => {
     sequelize.sync({ force: false }).then(() => done());
-  });
-
-  it('should GET /', async () => {
-    const res = await request(app).get('/');
-    expect(res.status).to.equal(200);
-    expect(res.body).to.not.be.empty;
-    expect(res.body).to.be.an('object');
-    expect(res.body.status).to.be.equal('error');
-    expect(res.body.reason).to.be.equal('Route not provided.');
   });
 
 });
