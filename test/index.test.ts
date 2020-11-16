694e048c06e1d3d1a4238689a720fa5f027b56a9
noskla <alis@krutt.org>
2020-11-16 15:24:44 +0100

    Force sync in database init test

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@ import sequelize from '../src/database';
 describe('Index Init Test', () => {
 
   it('should synchronize the database', done => {
-    sequelize.sync({ force: false }).then(() => done());
+    sequelize.sync({ force: true }).then(() => done());
   });
 
 });
