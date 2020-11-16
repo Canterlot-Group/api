f186855fba37a6d7ffdf7a8be2b82a475e6a0854
noskla <alis@krutt.org>
2020-11-16 13:50:15 +0100

    Enable debug tokens on CI tests, add authenticated GET user by id test

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,6 +1,7 @@
 import {expect} from 'chai';
 import app from '../src/app';
 import {agent as request} from 'supertest';
+import {User} from '../src/models/User';
 
 describe('Users', () => {
 
@@ -13,6 +14,14 @@ describe('Users', () => {
     expect(res.body.reason).to.be.equal('Unable to view other users without member privileges or higher.');
   });
 
-  // TODO: authenticated GET /user/:id
+  it('should not find any user, but pass the authentication process', async () => {
+    const res = await request(app).get('/user/61d93aa9-fbe4-4d8b-bf23-419dae1c0029')
+      .set('X-User-Token', '0000000000000000');
+    expect(res.status).to.equal(404);
+    expect(res.body).to.not.be.empty;
+    expect(res.body).to.be.an('object');
+    expect(res.body.status).to.be.equal('error');
+    expect(res.body.reason).to.be.equal('User by this ID does not exist.');
+  });
 
 });
