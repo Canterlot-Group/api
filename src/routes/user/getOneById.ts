914ca4816f93956086dd8c539d116670909fa000
noskla <alis@krutt.org>
2020-11-29 05:12:17 +0100

    Add activeSessions information to user by id route

--- src/routes/user/getOneById.ts
+++ src/routes/user/getOneById.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { User } from './../../models/User';
 import { Station } from './../../models/Station';
+import { Session } from './../../models/Session';
 
 /*
  * GET :id
@@ -11,7 +12,7 @@ export default function (req: Request, res: Response) {
   if (req.params.id !== res.locals.accountId && !['member', 'administrator'].includes(res.locals.accountType))
     return res.status(403).json({ status: 'error', reason: 'Unable to view other users without member privileges or higher.' });
 
-  User.findByPk(req.params.id, { attributes: ['id', 'name', 'email', 'loginEnabled', 'bannedUntil', 'bannedReason'], include: [Station] }).then(user => {
+  User.findByPk(req.params.id, { attributes: ['id', 'name', 'email', 'loginEnabled', 'bannedUntil', 'bannedReason'], include: [Station, Session] }).then(user => {
     if (user === null)
       return res.status(404).json({ status: 'error', reason: 'User by this ID does not exist.' });
     return res.json({ status: 'ok', user });
