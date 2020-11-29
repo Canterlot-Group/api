afa720baab66a2ecf0d3a57bd77b880958c32eec
noskla <alis@krutt.org>
2020-11-29 04:46:18 +0100

    Forbid some values in user creation route, fix check if user exist

--- src/routes/user/create.ts
+++ src/routes/user/create.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { User } from './../../models/User';
-import { ValidationErrorItem } from 'sequelize';
+import { ValidationErrorItem, Op } from 'sequelize';
 
 /*
  * POST - JSON
@@ -19,7 +19,9 @@ export default function (req: Request, res: Response) {
   if (res.locals.accountType !== 'administrator')
     return res.status(403).json({ status: 'error', reason: 'Ensure you\'re using an administrator token to create an account.' });
 
-  User.findOrCreate({where: req.body}).then(([user, created]: [any, boolean]) => {
+  ['id', 'bannedUntil', 'bannedReason', 'tokens', 'memberIn', 'avatar'].forEach(e => delete req.body[e]);
+
+  User.findOrCreate({where: {[Op.or]: {name: req.body.name, email: req.body.email}}}).then(([user, created]: [any, boolean]) => {
     if (!created)
       return res.status(400).json({ status: 'error', reason: 'User already exist.' });
     else
